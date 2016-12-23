af20878d913e035d23096969d55d3f6b31c49a7c
Ossey <xiaoyuan1314@me.com>
2016-12-23 20:28:53 +0800

    自定义导航右侧按钮

--- XYCustomNavigationBar/XYCustomNavigationBar/ViewController.m
+++ XYCustomNavigationBar/XYCustomNavigationBar/ViewController.m
@@ -20,6 +20,10 @@
     self.topBackgroundView.backgroundColor = [UIColor colorWithRed:211/255.0 green:45/255.0 blue:160/255.0 alpha:0.8];
     
     self.xy_title = @"自定义的导航条";
+    
+    // 设置右侧按钮
+    self.rightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.rightButton setTitle:@"右侧" forState:UIControlStateNormal];
 }
 
 
